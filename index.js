d0eecc54df36abb6faee9f1aa6f9a68fd9716db5
Marcus Sandi <tradedomarcus@gmail.com>
2023-03-16 15:07:35 -0300

    feat: setup session, flash messagens, res body and handlebars

--- index.js
+++ index.js
@@ -8,6 +8,53 @@ const { FileStore } = session;
 
 const app = e();
 
+//public path
+app.use(e.static("public"));
+
+//handlebars
+app.engine("handlebars", Handlebars.engine());
+app.set("view engine", "handlebars");
+
+//resposta do body
+app.use(
+	e.urlencoded({
+		extended: true,
+	})
+);
+
+app.use(e.json());
+
+//session middleware
+app.use(
+	session({
+		name: "session",
+		secret: "meu-secret",
+		resave: false,
+		saveUninitialized: false,
+		store: new FileStore({
+			logFn: function () {},
+			path: __dirname + "./sessions",
+		}),
+		cookie: {
+			secure: false,
+			maxAge: 360000,
+			expires: new Date(Data.now() + 360000),
+			httpOnly: true,
+		},
+	})
+);
+
+// flash messages
+app.use(flash());
+
+//set session  to response
+app.use((req, res, next) => {
+	if (req.session.userid) {
+		res.locals.session = req.session;
+	}
+
+	next();
+});
 
 sequelize
 	.sync()
